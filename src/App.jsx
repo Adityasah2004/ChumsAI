bae4233134270f828234dd4b0109bd2b8eb63041
Adityasah2004 <sahaditya59@gmail.com>
2024-01-06 00:06:37 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import Head from "./components/Navbar";
-import LandingPage from "./pages/LandingPage";
-import AboutPage from "./pages/About";
-import WhyUs from "./pages/WhyUs";
-import Blogs from "./pages/Blogs";
-import FAQ from "./components/FAQ";
-import Down from "./components/Footer";
-import "./App.css";
+import Head from './components/Navbar'
+import LandingPage from './pages/LandingPage';
+import AboutPage from './pages/About';
+import WhyUs from './pages/WhyUs';
+import Blogs from './pages/Blogs';
+import FAQ from './components/FAQ'
+import Down from './components/Footer'
+import './App.css'
+
 
 function App() {
+
   return (
     <>
-      <Head />
-      <LandingPage />
-      <AboutPage />
-      <WhyUs />
-      <Blogs />
-      <FAQ />
-      <Down />
-    </>
-  );
+      <Head/>
+      <LandingPage/>
+      <AboutPage/>
+      <WhyUs/>
+      <Blogs/>
+      <FAQ/>
+      <Down/>
+      </>
+  )
 }
 
-export default App;
+export default App
