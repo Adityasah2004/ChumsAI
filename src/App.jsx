0a3058fb85104fce0cf7a7bcbdf842760e233d53
Sahil Kumar <sahil.kumar@somaiya.edu>
2024-03-11 01:27:23 +0530

    [Changes & Added] Feature cards changed and added vercel analytics & speed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRoutes";
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import "./App.css";
 
 const App = () => {
+  injectSpeedInsights();
   return (
     <BrowserRouter>
       <AppRouter />
